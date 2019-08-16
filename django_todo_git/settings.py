f32b1e291741156cf60428af2dbec308332b6629
Ubuntu <jessica.devine@wpengine.com>
2019-08-16 19:32:45 +0000

    test

--- django_todo_git/settings.py
+++ django_todo_git/settings.py
@@ -26,7 +26,13 @@ SECRET_KEY = 'x$yhle@c=6mabtf2_j33m)!t-flepnk75qj7qttep*f6rvzkj7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['os.environ.get("HOSTNAME")']
+ALLOWED_HOSTS = [os.environ.get('HOSTNAME'),
+                'f752783af0ce4c4f8fcb80193862d0f3.vfs.cloud9.us-east-1.amazonaws.com']
+                
+                
+
+#ALLOWED_HOSTS = ['f752783af0ce4c4f8fcb80193862d0f3.vfs.cloud9.us-east-1.amazonaws.com',
+#                'simple-django-jess.herokuapp.com']
 
 #os.environ.get('MONGO_URI')
 
